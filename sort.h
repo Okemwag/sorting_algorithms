53be7de7c3db5d297ce5bbe911a3f0c9399465ab
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-21 18:23:34 +0300

    Add files via upload

---
+++ sort.h
@@ -0,0 +1,85 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+void swap(int *x, int *y);
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+/**
+ *enum kind_e - describes the kind
+ * @SPADE: the card
+ * @HEART: diff card
+ * @CLUB: one card
+ * @DIAMOND: check card
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+
+
+void sortedInsert(listint_t **head_ref, listint_t *newNode);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+int copy_elements(int *arr1, int *arr2, size_t size);
+int r_largest(int *array, size_t size);
+
+#endif
